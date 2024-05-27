d11aff9c87f68d388196c9cec091ed2e1c5be687
Theo2903 <theo@ribbi.ch>
2024-05-27 11:23:05 +0200

    Update GetStarted screen

--- src/Screens/GetStarted/GetStarted.js
+++ src/Screens/GetStarted/GetStarted.js
@@ -1,15 +1,86 @@
-// UI & Component
 import { View, Text, StyleSheet } from "react-native";
 import { Container } from "../../Components/Container/Container";
+import LottieView from "lottie-react-native";
+import HeroAnimation from "../../../assets/animations/hero_animation.json";
+import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 export const GetStarted = () => {
+  const navigation = useNavigation();
+  const handleOnPressBegin = () => {
+    navigation.navigate("SelectDiet");
+  };
   return (
-    <Container>
-      <Text>Template</Text>
-    </Container>
+    <View style={s.container}>
+      <Container>
+        <View style={s.headerContainer}>
+          <Text style={s.title}>BeFit</Text>
+        </View>
+        <LottieView source={HeroAnimation} style={s.animation} autoPlay loop />
+      </Container>
+      <View style={s.drawer}>
+        <Container>
+          <View>
+            <Text style={{ fontWeight: "bold", fontSize: 18, marginTop: 10 }}>
+              Bienvenue sur BeFit !
+            </Text>
+            <Text style={{ marginTop: 20 }}>
+              BeFit est une application qui vous aide à surveiller votre régime
+              alimentaire en scannant vos aliments. Gardez un œil sur votre
+              santé et atteignez vos objectifs nutritionnels facilement avec
+              BeFit.
+            </Text>
+          </View>
+        </Container>
+        <Button
+          title="Commencer !"
+          type="outline"
+          onPress={handleOnPressBegin}
+          titleStyle={{ fontWeight: "bold", fontSize: 23 }}
+          buttonStyle={{
+            borderRadius: 20,
+            paddingVertical: 15,
+            paddingHorizontal: 40,
+          }}
+          containerStyle={{
+            width: "80%",
+            alignSelf: "center",
+            marginTop: 30,
+          }}
+        />
+      </View>
+    </View>
   );
 };
 
 const s = StyleSheet.create({
-  template: {},
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  container: {
+    height: "100%",
+  },
+  headerContainer: {
+    alignItems: "center",
+  },
+  animation: {
+    height: "50%",
+    width: "100%",
+    alignItems: "center",
+  },
+  headerText: {
+    flex: 1,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  drawer: {
+    height: "50%",
+    backgroundColor: "#FFFF",
+    borderColor: "#FFFF",
+    borderWidth: 5,
+    borderRadius: 30,
+    paddingVertical: 20,
+  },
 });
