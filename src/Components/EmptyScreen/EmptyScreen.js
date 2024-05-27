91bb168171e6134ac4ef738c6d17b08640fd41c5
Theo2903 <theo@ribbi.ch>
2024-05-27 11:22:14 +0200

    Update EmptyScreen with button

--- src/Components/EmptyScreen/EmptyScreen.js
+++ src/Components/EmptyScreen/EmptyScreen.js
@@ -2,22 +2,35 @@
 import { View, Text, StyleSheet } from "react-native";
 import LottieView from "lottie-react-native";
 import FoodAnimation from "../../../assets/animations/food_animation.json";
+import { Button } from "react-native-elements";
 
-export const EmptyScreen = () => {
+export const EmptyScreen = ({ btnLabel, onPress }) => {
   return (
     <View style={s.container}>
       <LottieView source={FoodAnimation} style={s.animation} autoPlay loop />
+      {btnLabel && (
+        <Button
+          title={btnLabel}
+          type="outline"
+          style={s.btn}
+          onPress={onPress}
+        />
+      )}
     </View>
   );
 };
 
 const s = StyleSheet.create({
   container: {
-    paddingBottom: 200
+    paddingBottom: 200,
   },
   animation: {
     height: "100%",
     width: "100%",
     alignItems: "center",
+  },
+  btn: {
+    paddingLeft: 15,
+    paddingRight: 15,
   },
 });
